acfe8baea65ef831cbcfbc40eb23d8cec0db2a40
Akshar2007 <88934310+Akshar2007@users.noreply.github.com>
2025-11-08 10:10:47 +0530

    Add program to input and display book names

---
+++ three book .c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    char books[3][50];
+    char *ptr[3];
+    int i;
+
+    printf("Enter names of 3 books:\n");
+    for(i=0; i<3; i++) {
+        scanf(" %[^\n]", books[i]);
+        ptr[i] = books[i];
+    }
+
+    printf("\nBook names are:\n");
+    for(i=0; i<3; i++)
+        printf("%s\n", ptr[i]);
+
+    return 0;
+}
